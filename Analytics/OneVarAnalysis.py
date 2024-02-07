9944275f4114757b8944f7e0877d848af2b5f719
Loomthie <mcfarldl@mail.uc.edu>
2024-02-07 14:09:08 -0500

    Merge remote-tracking branch 'origin/master'

---
+++ Analytics/OneVarAnalysis.py
@@ -0,0 +1,5 @@
+class DistributionAnalysis:
+
+    def __init__(self,name,dataset):
+        self.name = name
+        self.dataset = dataset
--- LabTests/TLC.py
+++ LabTests/TLC.py
@@ -1,5 +1,5 @@
 import urllib.request as re
-from IPython.display import display_html
+import numpy as np
 
 
 class Spot:
@@ -36,5 +36,37 @@ class Plate:
     def _repr_html_(self):
         cnt = "".join([i.decode('utf-8') for i in re.urlopen(self.__templateFile)])
         tlcSpots = "[" + ",".join([str(i) for i in self.args]) + "]"
+        return cnt.format(tlcSpots=tlcSpots)
+
+
+class SeriesAvg:
+
+    def __init__(self,series_name, **spots:list[Spot]):
+        self.seriesName = series_name
+        self.spots = {}
+        for key,val in spots.items():
+            rfVals = [i.rfValue for i in val]
+            self.spots[key] = [np.average(rfVals),np.std(rfVals)]
+
+    def __iter__(self):
+        return (i for i in [self.seriesName,[self.spots[key][0] for key in self.spots],[self.spots[key][1] for key in self.spots]])
+
+    def __str__(self):
+        return str(list(self))
+
+
+class PlateAvg:
+
+    __templateFile = ""
+
+    def __init__(self,*series_avgs:SeriesAvg):
+        self.series = series_avgs
+
+    def _repr_html_(self):
+        # with open("TLCAvg_template.html",'r') as file:
+        #     cnt = file.read().format(tlcSpots="[" + ",".join([str(i) for i in self.series]) + "]")
+
+        cnt = "".join([i.decode('utf-8') for i in re.urlopen(self.__templateFile)])
+        tlcSpots = "[" + ",".join([str(i) for i in self.series]) + "]"
         return cnt.format(tlcSpots=tlcSpots)
 
